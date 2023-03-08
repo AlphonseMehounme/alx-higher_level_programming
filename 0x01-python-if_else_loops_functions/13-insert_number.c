7d668be6d28ccb76ea71166cbc7106d6172e0e28
Alphonse Mehounme <alphonsemehounme9@gmail.com>
2023-03-07 16:51:36 -0800

    Python if else loops function files

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,12 @@
+#include <lists.h>
+
+/**
+ * listint_t - function that inserts a number into a sorted singly linked list
+ * @number: int variable
+ * Return: the address of the new node, or NULL if it failed
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+
+}
