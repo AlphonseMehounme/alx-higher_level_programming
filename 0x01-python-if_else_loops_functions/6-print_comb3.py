4fe43c4471c6ecb81d099fa8976a5a83496ba238
Alphonse Mehounme <alphonsemehounme9@gmail.com>
2023-03-08 18:58:36 -0800

    Combine

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,7 +4,7 @@ for i in range(1, 10):
 for i in range(1, 8):
     j = i + 1
     while j < 10:
-        number = i*10 + j 
+        number = i * 10 + j
         j = j + 1
         print("{}, ".format(number), end='')
 print("{}".format(89))
