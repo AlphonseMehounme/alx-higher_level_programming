1bd590ada9714a436240ded287328f60ac7f4cc4
Alphonse Mehounme <alphonsemehounme9@gmail.com>
2023-03-08 18:54:10 -0800

    Combinaison

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+for i in range(1, 10):
+    print("{:02d}, ".format(i), end='')
+for i in range(1, 8):
+    j = i + 1
+    while j < 10:
+        number = i*10 + j 
+        j = j + 1
+        print("{}, ".format(number), end='')
+print("{}".format(89))
