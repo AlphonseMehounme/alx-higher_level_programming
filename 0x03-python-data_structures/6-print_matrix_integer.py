f6e98d6501f85440834420fd7458f0b8b05bf4fb
AlphonseMehounme <alphonsemehounme9@gmail.com>
2023-11-03 20:49:22 -0700

    Handle space between integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for rown in row:
-            print("{:d}".format(rown), end='')
-            print(" ".format(), end='')
+        for j in range(len(row)):
+            print("{:d}".format(row[j]), end='')
+            if j < len(row) -1:
+                print(" ".format(), end='')
         print("")
