ac58295ea8437e6342a858e8d0c72673c7a10279
AlphonseMehounme <alphonsemehounme9@gmail.com>
2023-11-06 12:15:24 -0800

    Delete reverse and loop through from the end

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
-    for i in my_list:
-        print("{}".format(i))
+    ln = len(my_list) - 1
+    for i in range(ln, -1, -1):
+        print("{:d}".format(my_list[i]))
