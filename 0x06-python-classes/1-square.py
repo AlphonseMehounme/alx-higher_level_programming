7ae5a3969c3146229aa42d818cc60735f04a2bad
AlphonseMehounme <alphonsemehounme9@gmail.com>
2023-11-22 18:36:17 -0800

    Definition of square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Simple square module
+
+This is a simple module to start class
+
+"""
+
+
+class Square:
+    """Square
+
+    This is squre class
+
+    """
+    def __init__(self, size):
+        """Init function
+
+        This is the init function
+
+        Args:
+            size (int): Size of square
+
+        """
+        self.__size = size
