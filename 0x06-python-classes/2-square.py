a86273f710298188e43cf65046d7f444b6097188
AlphonseMehounme <alphonsemehounme9@gmail.com>
2023-11-22 19:13:09 -0800

    Fix error

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -23,7 +23,7 @@ class Square:
         """
         try:
             self.__size = size
+            if size < 0:
+                raise ValueError("size must be >= 0")
         except TypeError:
             print("size must be an integer".format())
-        if size < 0:
-            raise ValueError("size must be >= 0")
