89dafa846aabfcf08efc632091000847fc3bee4a
AlphonseMehounme <alphonsemehounme9@gmail.com>
2023-11-22 18:56:11 -0800

    Introduce try

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Simple Square Module
+
+This is a simple module introducing to class
+
+"""
+
+
+class Square:
+    """Square class
+
+    This is the square class
+
+    """
+    def __init__(self, size=0):
+        """Init function
+
+        This is Square init function
+        It intanciate the class
+
+        Args:
+            size (int): Size of the squre
+        """
+        try:
+            self.__size = size
+        except TypeError:
+            print("size must be an integer".format())
+        if size < 0:
+            raise ValueError("size must be >= 0")
