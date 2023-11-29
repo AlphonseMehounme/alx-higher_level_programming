417952f88037b7fc4a8f3914ca1bae17d602acbc
AlphonseMehounme <alphonsemehounme9@gmail.com>
2023-11-29 09:10:13 -0800

    Raise TypeError

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -22,8 +22,8 @@ class Square:
             size (int): Size of the squre
         """
         try:
-            self.__size = int(size)
-            if int(size) < 0:
+            self.__size = size
+            if size < 0:
                 raise ValueError("size must be >= 0")
-        except (TypeError):
-            print("size must be an integer".format())
+        except TypeError:
+            raise TypeError("size must be an integer")
