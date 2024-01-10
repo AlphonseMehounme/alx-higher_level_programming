184cb658a89d507492db5336a1668909e462ee37
AlphonseMehounme <alphonsemehounme9@gmail.com>
2024-01-09 17:45:16 -0800

    Update

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -85,9 +85,9 @@ class Rectangle:
         """
         if self.__width == 0 or self.__height == 0:
             return ""
-        prt = ""
+        prt = str(Rectangle.print_symbol)
         for i in range(self.__height):
-            for j in range(self.__width):
+            for j in range(self.__width - 1):
                 prt += str(Rectangle.print_symbol)
             if i != self.__height - 1:
                 prt += "\n"
