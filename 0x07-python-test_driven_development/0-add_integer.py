9301822425855c9e10bc05f191e35eef00820770
AlphonseMehounme <alphonsemehounme9@gmail.com>
2024-01-15 02:29:35 -0800

    Add infinite case

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -14,10 +14,14 @@ def add_integer(a, b=98):
     """
     try:
         a = math.floor(a)
+        if a + 1 == a:
+            raise OverflowError("Float overflow")
     except TypeError:
         raise TypeError("a must be an integer")
     try:
         b = math.floor(b)
+        if b + 1 == b:
+            raise OverflowError("Float overflow")
     except TypeError:
         raise TypeError("b must be an integer")
     return a + b
