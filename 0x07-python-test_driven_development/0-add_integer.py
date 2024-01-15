2074ce46406caea8de66ba9e6fe9b6bed2eba5d5
AlphonseMehounme <alphonsemehounme9@gmail.com>
2024-01-15 04:36:00 -0800

    Add type check test

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -12,16 +12,11 @@ def add_integer(a, b=98):
     """
         Add integer function
     """
-    try:
-        a = math.floor(a)
-        if a + 1 == a:
-            raise OverflowError("Float overflow")
-    except TypeError:
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
+    a = math.floor(a)
     try:
         b = math.floor(b)
-        if b + 1 == b:
-            raise OverflowError("Float overflow")
     except TypeError:
         raise TypeError("b must be an integer")
     return a + b
