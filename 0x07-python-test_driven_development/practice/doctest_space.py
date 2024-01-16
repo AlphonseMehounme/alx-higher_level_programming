7eb10c066560c211fe7fdca93a07d8234d573951
AlphonseMehounme <alphonsemehounme9@gmail.com>
2024-01-16 14:33:43 -0800

    Handling whitespace with doctest

---
+++ 0x07-python-test_driven_development/practice/doctest_space.py
@@ -0,0 +1,9 @@
+def my_function(a, b):
+    """Returns a * b.
+
+    >>> my_function(['A', 'B'], 3) #doctest: +NORMALIZE_WHITESPACE
+    ['A', 'B',
+     'A', 'B',
+     'A', 'B']
+    """
+    return a * b
