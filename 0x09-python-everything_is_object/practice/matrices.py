ddd2018a5424e53376b87be31c130fd95c52226d
AlphonseMehounme <alphonsemehounme9@gmail.com>
2024-01-25 16:38:12 -0800

    Update answer 16

--- 0x09-python-everything_is_object/practice/matrices.py
+++ 0x09-python-everything_is_object/practice/matrices.py
@@ -19,3 +19,40 @@ def add_row(matrix):
     newrow = [[0] * len(newmat[0])]
     newmat += newrow
     return newmat
+
+def add_column(matrix):
+    """
+      >>> m = [[0, 0], [0, 0]]
+      >>> add_column(m)
+      [[0, 0, 0], [0, 0, 0]]
+      >>> n = [[3, 2], [5, 1], [4, 7]]
+      >>> add_column(n)
+      [[3, 2, 0], [5, 1, 0], [4, 7, 0]]
+      >>> n
+      [[3, 2], [5, 1], [4, 7]]
+    """
+    newmat = copy.deepcopy(matrix)
+    for i in range(len(newmat)):
+        newmat[i] += [0]
+    return newmat
+
+def add_matrices(m1, m2):
+    """
+      >>> a = [[1, 2], [3, 4]]
+      >>> b = [[2, 2], [2, 2]]
+      >>> add_matrices(a, b)
+      [[3, 4], [5, 6]]
+      >>> c = [[8, 2], [3, 4], [5, 7]]
+      >>> d = [[3, 2], [9, 2], [10, 12]]
+      >>> add_matrices(c, d)
+      [[11, 4], [12, 6], [15, 19]]
+      >>> c
+      [[8, 2], [3, 4], [5, 7]]
+      >>> d
+      [[3, 2], [9, 2], [10, 12]]
+   """
+   newmat = copy.deepcopy(m1)
+   for i in range(len(m1)):
+       for in range(len(m[1])):
+           newmat[i][j] = m1[i][j] + m2[i][j]
+return newmat
