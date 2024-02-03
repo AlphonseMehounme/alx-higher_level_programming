73eca4ab5e6a978ab3f6959c82a410f5c64d2b90
AlphonseMehounme <alphonsemehounme9@gmail.com>
2024-02-03 15:25:50 -0800

    Implement area and str function for Rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+  Rectangle Module
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+      Class Rectangle
+    """
+    def __init__(self, width, height):
+        """
+          Instanciation function
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """
+           Calculate area of a rectangle
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+          Define the print format of a rectangle object
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
