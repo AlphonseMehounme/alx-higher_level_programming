13e3164eb4b221a306ab48768662eb6aad1ea1e8
AlphonseMehounme <alphonsemehounme9@gmail.com>
2024-02-17 16:26:17 -0800

    From json

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/sur/bin/python3
+"""
+  From Json Module
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+      Return a python object from json string
+    """
+    return json.loads(my_str)
