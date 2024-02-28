90ae588d8a03b7ab02ee1d92e8b5f71cdcbe0156
AlphonseMehounme <alphonsemehounme9@gmail.com>
2024-02-28 14:25:00 -0800

    Write and read

---
+++ 0x0B-python-input_output/practice/writeandread.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+  Write and Read Module
+"""
+
+
+with open('test.log', mode='w', encoding='utf-8') as f:
+    f.write('test succeeded')
+with open('test.log', mode='r', encoding='utf-8') as f:
+    print(f.read())
+with open('test.log', mode='a', encoding='utf-8') as f:
+    f.write('and again')
+with open('test.log', encoding='utf-8') as f:
+    print(f.read())
