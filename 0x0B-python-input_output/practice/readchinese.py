3af43cebb106f61bf2e4f3c2070c13ed461fab31
AlphonseMehounme <alphonsemehounme9@gmail.com>
2024-02-28 13:11:05 -0800

    Add line reading

--- 0x0B-python-input_output/practice/readchinese.py
+++ 0x0B-python-input_output/practice/readchinese.py
@@ -7,3 +7,9 @@
 with open('chinese.txt', encoding='utf-8') as f:
     content = f.read()
     print(content)
+
+line_num = 0
+with open('online.txt', encoding='utf-8') as fx:
+    for line in fx:
+        line_num += 1
+        print('{:>4} {}'.format(line_num, line))
