bbd6915a35e1253509de0a3eda286144dc5d35e4
AlphonseMehounme <alphonsemehounme9@gmail.com>
2024-03-02 15:03:28 -0800

    Update

--- 0x0B-python-input_output/practice/writeandread.py
+++ 0x0B-python-input_output/practice/writeandread.py
@@ -7,8 +7,9 @@
 with open('test.log', mode='w', encoding='utf-8') as f:
     f.write('test succeeded')
 with open('test.log', mode='r', encoding='utf-8') as f:
-    print(f.read())
+    content = f.read()
+    print(content)
 with open('test.log', mode='a', encoding='utf-8') as f:
-    f.write('and again')
+    f.write(content)
 with open('test.log', encoding='utf-8') as f:
     print(f.read())
