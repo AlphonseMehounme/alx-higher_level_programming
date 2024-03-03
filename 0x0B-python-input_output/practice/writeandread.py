9be20d1c070271f365add8222ae3e839fc4fcb6f
AlphonseMehounme <alphonsemehounme9@gmail.com>
2024-03-03 07:58:51 -0800

    Using os

--- 0x0B-python-input_output/practice/writeandread.py
+++ 0x0B-python-input_output/practice/writeandread.py
@@ -2,6 +2,7 @@
 """
   Write and Read Module
 """
+import os
 
 
 with open('test.log', mode='w', encoding='utf-8') as f:
@@ -13,3 +14,8 @@ with open('test.log', mode='a', encoding='utf-8') as f:
     f.write(content)
 with open('test.log', encoding='utf-8') as f:
     print(f.read())
+os.rename('test.log', 'newtest.log')
+with open('newtest.log', 'r', encoding='utf-8') as f:
+    print(f.readline())
+os.mkdir('newdir')
+print('Current w dir :', os.getcwd())
