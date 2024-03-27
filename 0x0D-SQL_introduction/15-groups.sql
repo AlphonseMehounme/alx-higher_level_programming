3c7d531590aa58cb3996233d97adeceddc8f65f4
AlphonseMehounme <alphonsemehounme9@gmail.com>
2024-03-27 06:21:35 -0700

    Update alias number

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- lists the number of records with the same score in the table second_table
 -- We use SELECT and GROUP BY
-SELECT score, COUNT(score) AS "number" FROM second_table GROUP BY score ORDER BY number DESC;
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY number DESC;
