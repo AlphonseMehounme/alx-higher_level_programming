d7f879d2867210b9302cb70ac388035d42b3cb6a
AlphonseMehounme <alphonsemehounme9@gmail.com>
2024-03-27 16:32:44 -0700

    Create user and grand permissions

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- CREATE USER user_0d_1 with all privileges
 -- We use CREATE USER and add privileges
 CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO user_0d_1;
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
