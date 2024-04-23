4a9826cc7b43b50044074b2a5827e3ce77a5bcd2
AlphonseMehounme <alphonsemehounme9@gmail.com>
2024-04-23 05:03:29 -0700

    console log factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+
+function factorial(a) {
+  if (a === 0) {
+    return 1;
+  } else {
+    return a * factorial(a - 1);
+  }
+}
+
+let number;
+if (isNaN(argv[2])) {
+  number = 0;
+} else {
+  number = parseInt(argv[2]);
+}
+
+console.log(factorial(number));
