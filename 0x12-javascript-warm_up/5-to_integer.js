8202e943acc0ce8830f4b95ee93e8420e2f3d562
AlphonseMehounme <alphonsemehounme9@gmail.com>
2024-04-22 20:01:32 -0700

    Print string via a loop

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,5 +5,5 @@ const { argv } = require('node:process');
 if (isNaN(argv[2])) {
   console.log('Not a number');
 } else {
-  console.log('My number: ' + Number(argv[2]));
+  console.log('My number: ' + parseInt(argv[2]));
 }
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const lang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let step = 0; step < 3; step++) {
+  console.log(lang[step]);
+}
