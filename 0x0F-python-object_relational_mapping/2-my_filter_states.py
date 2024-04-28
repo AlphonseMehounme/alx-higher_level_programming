c1d2bc4a33788d2cdbe74cb03cd47772a9c3d582
AlphonseMehounme <alphonsemehounme9@gmail.com>
2024-04-28 04:23:33 -0700

    State by name

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 """
-  State with N starting
+  State by name
 """
 import sys
 import MySQLdb
+
 
 if __name__ == "__main__":
     user = sys.argv[1]
     passwd = sys.argv[2]
     dtbase = sys.argv[3]
+    cityName = sys.argv[4]
 
-    db = MySQLdb.connect(host="localhost", user=user,
-                         passwd=passwd, db=dtbase)
+    db = MySQLdb.connect(host="localhost", user=user, passwd=passwd, db=dtbase)
     cur = db.cursor()
-
-    cur.execute('SELECT * FROM states WHERE name LIKE "N%" ORDER BY id ASC')
+    cur.execute('SELECT * FROM states WHERE name="{}"'.format(cityName))
     states = cur.fetchall()
 
     for state in states:
