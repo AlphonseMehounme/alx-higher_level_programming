fc4e6aaf5a8a090a7985c0e9345426ec09053edd
AlphonseMehounme <alphonsemehounme9@gmail.com>
2024-04-28 02:25:39 -0700

    Select states from states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,3 +1,20 @@
--- lists all states from the database hbtn_0e_0_usa
+#!/usr/bin/python3
+"""
+  Select States Module
+"""
+import MySQLdb
+import sys
 
-SELECT * FROM states;
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    passwd = sys.argv[2]
+    dtbase = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost", user=username, passwd=passwd, db=dtbase)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id")
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
