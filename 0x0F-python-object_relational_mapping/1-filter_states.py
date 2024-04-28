2bbef9ea38f78d5e0f3408ce50869df885deb077
AlphonseMehounme <alphonsemehounme9@gmail.com>
2024-04-28 05:03:53 -0700

    Change username to user

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,19 +2,19 @@
 """
   State with N starting
 """
-import sys
 import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
-    user = sys.argv[1]
+    username = sys.argv[1]
     passwd = sys.argv[2]
     dtbase = sys.argv[3]
 
-    db = MySQLdb.connect(host="localhost", user=user,
+    db = MySQLdb.connect(host="localhost", user=username,
                          passwd=passwd, db=dtbase)
     cur = db.cursor()
-
-    cur.execute('SELECT * FROM states WHERE name LIKE "N%" ORDER BY id ASC')
+    cur.execute('SELECT * FROM states WHERE name LIKE "N%" ORDER BY id')
     states = cur.fetchall()
 
     for state in states:
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -2,17 +2,18 @@
 """
   State by name
 """
-import sys
 import MySQLdb
+import sys
 
 
 if __name__ == "__main__":
-    user = sys.argv[1]
+    username = sys.argv[1]
     passwd = sys.argv[2]
     dtbase = sys.argv[3]
     cityName = sys.argv[4]
 
-    db = MySQLdb.connect(host="localhost", user=user, passwd=passwd, db=dtbase)
+    db = MySQLdb.connect(host="localhost", user=username,
+                         passwd=passwd, db=dtbase)
     cur = db.cursor()
     cur.execute('SELECT * FROM states WHERE name="{}"'.format(cityName))
     states = cur.fetchall()
