bf89fb83fa616cc6041d007a4821039afc5af8fd
AlphonseMehounme <alphonsemehounme9@gmail.com>
2024-04-30 07:55:05 -0700

    List all states from hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,22 +2,17 @@
 """
   Model Module
 """
-import MySQLdb
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-# from sqlalchemy import (create_engine)
 
 
+# Base class
 Base = declarative_base()
-
 
 class State(Base):
     """
       State class
     """
     __tablename__ = 'states'
-    id = Column(Integer, autoincrement=True, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-
-
-# engine = create_engine('mysql+mysqldb://root:root@localhost/hbtn_0e_6_usa')
