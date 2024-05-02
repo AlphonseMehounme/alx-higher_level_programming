abb29316cabd76f946f847e8e9e97e4fbd1a327b
AlphonseMehounme <alphonsemehounme9@gmail.com>
2024-05-02 15:59:09 -0700

    Add session

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -2,9 +2,8 @@
 """
   New City
 """
-from model_state import Base
 from relationship_city import City
-from relationship_state import State
+from relationship_state import State, Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import sys
@@ -13,3 +12,5 @@ import sys
 if __name__ == "__main__":
     engine = create_engine("mysql+lysqldb://{}:{}@localhost/{}".format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
