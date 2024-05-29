4baa0f40d26eb73b18688305c4b5ed0f63d1eeea
Alphonse <alphonsemehounme9@gmail.com>
2024-05-29 02:16:38 -0700

    Add POST

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send POST request and print response body
-curl -s -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1" # -X POST "$1"
+curl -s -d "email=test@gmail.com" -d "subject=I will always be here for PLD" -X POST "$1"
