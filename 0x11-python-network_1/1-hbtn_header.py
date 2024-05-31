54880b71790b75fcf0b572d581ca82e96c1387bb
Alphonse <alphonsemehounme9@gmail.com>
2024-05-31 06:27:14 -0700

    new way to get request ID

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 """
-  Send request
+  Send request to provided url
 """
 import urllib.request
+import sys
 
 
-req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+url = sys.argv[1]
+req = urllib.request.Request(url)
 with urllib.request.urlopen(req) as response:
-    response_bytes = response.read()
-    print("Body response:")
-    print(f"\t- type: {type(response_bytes)}")
-    print(f"\t- content: {response_bytes}")
-    print(f"\t- utf8 content: {response_bytes.decode('utf-8')}")
+    headers = response.getheaders()
+    for header in headers:
+        if header[0] == 'X-Request-Id':
+            print(header[1])
+    #print("Body response:")
+    #print(f"\t- type: {type(response_bytes)}")
+    #print(f"\t- content: {response_bytes}")
+    #print(f"\t- utf8 content: {response_bytes.decode('utf-8')}")
