261f815e6588a70987d7c3b9fca68d26c992af17
Alphonse <alphonsemehounme9@gmail.com>
2024-05-31 06:42:29 -0700

    Avoid exec at import

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,10 +7,14 @@ import urllib.request
 import sys
 
 
-url = sys.argv[1]
-req = urllib.request.Request(url)
-with urllib.request.urlopen(req) as response:
-    headers = response.getheaders()
-    for header in headers:
-        if header[0] == 'X-Request-Id':
-            print(header[1])
+if '__name__': '__main__':
+    """
+      Main section
+    """
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        headers = response.getheaders()
+        for header in headers:
+            if header[0] == 'X-Request-Id':
+                print(header[1])
