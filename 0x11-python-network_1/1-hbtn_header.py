41100c59f75361631dd98d50d921297e1d6c1079
Alphonse <alphonsemehounme9@gmail.com>
2024-06-03 13:39:45 -0700

    Update documentation

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,17 +2,12 @@
 """
 This module Send request to provided url from commmand Line and print the
 value of X-Request-ID header to standard output
-Usage : ./1-hbtn_header.py https://alx-intranet.hbtn.io
 """
 import urllib.request
 import sys
 
 
-if __name__ == "__main__":
-    """
-      Main section that uses the provided URL to send request and print value
-      of X-Request-ID header to ouptut
-    """
+if __name__ == "__main__": 
     url = sys.argv[1]
     req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as response:
