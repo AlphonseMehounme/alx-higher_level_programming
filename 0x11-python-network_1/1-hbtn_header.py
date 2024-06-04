e8917b918d6314fbd2c919e969b6d4bd29371207
Alphonse <alphonsemehounme9@gmail.com>
2024-06-04 06:56:42 -0700

    Fix pycodestyle error on l10

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,7 +7,7 @@ import urllib.request
 import sys
 
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     url = sys.argv[1]
     req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as response:
