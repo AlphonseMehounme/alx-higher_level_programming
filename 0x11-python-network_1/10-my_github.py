9e907e0f72a96354a88bb723c8a9aea480a6289f
Alphonse <alphonsemehounme9@gmail.com>
2024-06-07 04:48:13 -0700

    Fix: case wrong passwd

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,11 +8,14 @@ import requests
 
 
 if __name__ == "__main__":
-    url = "https://api.github.com/users/" + sys.argv[1]
+    url = "https://api.github.com/user" #+ sys.argv[1]
     headers = {
             "Accept": "application/vnd.github+json",
             "Authorization": "Bearer " + sys.argv[2],
             "X-GitHub-Api-Version": "2022-11-28"
             }
-    resp = requests.get(url, headers)
-    print(resp.json()['id'])
+    resp = requests.get(url, headers=headers)
+    try:
+        print(resp.json()['id'])
+    except KeyError:
+        print(None)
