1a0725c27a673b4851131bd34fed84fafc63f751
Alphonse <alphonsemehounme9@gmail.com>
2024-06-07 00:57:53 -0700

    Add commit author name

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,13 +7,14 @@ import requests
 
 
 if __name__ == "__main__":
-    url = "https://api.github.com/repos/" + sys.argv[2] + "/" + sys.argv[1] + "/commits"
+    url = "https://api.github.com/repos/" + sys.argv[2] + "/" + sys.argv[1] +\
+            "/commits"
     headers = {
             "Accept": "application/vnd.github+json",
             "Authorization": "Bearer",
             "X-GitHub-Api-Version": "2022-11-28"
           }
     com = requests.get(url, headers)
-    for i in range(-1, -10, -1):
-        print(com.json()[i]['sha'])
-    print(com.status_code)
+    for i in range(10):
+        print(f"{com.json()[i]['sha']}:\
+              {com.json()[i]['commit']['author']['name']}")
