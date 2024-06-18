d96a8f315ac1da9c7a168819b12ab827fce1b86f
AlphonseMehounme <alphonsemehounme9@gmail.com>
2024-06-18 19:27:17 +0100

    Fetch data and display content

--- 0x15-javascript-web_jquery/3-script.js
+++ 0x15-javascript-web_jquery/3-script.js
@@ -1,1 +1,1 @@
-$("DIV#red_header").on('click', function() { $("header").addClass("red") });
+$('DIV#red_header').on('click', function () { $('header').addClass('red'); });
--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,5 +1,4 @@
-$("DIV#toggle_header").on("click", function() {
-		$("header").toggleClass("green");
-		$("header").toggleClass("red");
-	}
-);
+$('DIV#toggle_header').on('click', function () {
+  $('header').toggleClass('green');
+  $('header').toggleClass('red');
+});
