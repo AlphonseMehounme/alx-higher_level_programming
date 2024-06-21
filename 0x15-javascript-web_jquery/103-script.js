3a72ed65f7797226ade2ae8ebfbb237d151cae80
AlphonseMehounme <alphonsemehounme9@gmail.com>
2024-06-21 16:26:44 +0100

    Fix semistandard error on 103-script.js

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
-  $('INPUT#btn_translate').on('click', function () { 
-    var lang = $('INPUT#language_code').val();
+  $('INPUT#btn_translate').on('click', function () {
+    const lang = $('INPUT#language_code').val();
     $.get('https://hellosalut.stefanbohacek.dev/?lang=' + lang, function (data, status) {
       $('DIV#hello').append(data.hello);
     });
   });
   $('INPUT#language_code').on('keydown', function (event) {
     if (event.key === 'Enter') {
-      var lang = $('INPUT#language_code').val();
+      const lang = $('INPUT#language_code').val();
       $.get('https://hellosalut.stefanbohacek.dev/?lang=' + lang, function (data, status) {
         $('DIV#hello').append(data.hello);
       });
